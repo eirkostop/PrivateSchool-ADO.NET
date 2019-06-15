5080226919a2218f17acd9e8470121a6f270ce96
Eirini Kostopoulou <Eirini.Kostopoulou@Peoplecert.onmicrosoft.com>
2019-06-15 12:55:34 +0300

    moved printable list

--- assignment2/DbManager.cs
+++ assignment2/DbManager.cs
@@ -7,6 +7,45 @@ using System.Data.SqlClient;
 
 namespace assignment2
 {
+
+    class PrintableList<T> : List<T>
+    {
+        public override string ToString()
+        {
+            string rows = string.Empty;
+            rows += "\n----------------------------------------------------------------------------------------\n";
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                rows += string.Format(" {0,-18}", prop.Name);
+            }
+            rows += "\n----------------------------------------------------------------------------------------";
+
+            foreach (T o in this)
+            {
+                rows += "\n";
+                foreach (var prop in o.GetType().GetProperties())
+                {
+                    if (prop.PropertyType == typeof(DateTime))
+                    {
+                        rows += string.Format(" {0,-18:d}", prop.GetValue(o));
+                    }
+                    else if (prop.PropertyType == typeof(Decimal))
+                    {
+                        rows += string.Format(" {0,-18:c}", prop.GetValue(o));
+                    }
+                    else
+                    {
+                        rows += string.Format(" {0,-18}", prop.GetValue(o));
+                    }
+                }
+            }
+            if (this.Count == 0)
+            {
+                rows += "\n No entries";
+            }
+            return rows;
+        }
+    }
     class DbManager
     {
         public string SuccessMesssage { get; private set; }
